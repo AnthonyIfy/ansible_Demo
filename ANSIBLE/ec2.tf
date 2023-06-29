3957c4d07e888bf657450bf48e8f386f49e27a80
AnthonyIfy <136186728+AnthonyIfy@users.noreply.github.com>
2023-06-29 18:55:42 -0500

    Update ec2.tf

--- ANSIBLE/ec2.tf
+++ ANSIBLE/ec2.tf
@@ -97,7 +97,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.medium"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group2.id]
-  key_name               = "new-zone"
+  key_name               = "private-joy"
   count                  = 4
 
   tags = {
